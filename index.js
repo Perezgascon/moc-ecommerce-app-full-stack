fa15bb5845a8a2033beee7ade705ce2318a402a9
Perezgascon <76476520+Perezgascon@users.noreply.github.com>
2024-02-14 12:47:27 +0700

    established API connection and seeded database

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 require('dotenv').config();
 const { testConnection } = require('./db/conn');
+const { Product } = require('./models/productsModel');
 
 
 const app = express();
@@ -12,6 +13,17 @@ testConnection();
 
 app.get('/health', (req, res) => {
     res.send("It's Alive!");
+});
+
+//testing connection
+
+app.get('/products', async (req, res) => { // Make the route handler asynchronous
+    try {
+        const products = await Product.findAll(); // Wait for the Promise to resolve
+        res.status(200).json(products); // Send the response with the products
+    } catch (err) {
+        res.status(500).json({ message: err.message });
+    }
 });
 
 app.listen(PORT, () => {
