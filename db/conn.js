51498d6d2fd5f76c2d4d6c2d27478b81d5169c6e
Perezgascon <76476520+Perezgascon@users.noreply.github.com>
2024-02-14 11:01:12 +0700

    connected to database

---
+++ db/conn.js
@@ -0,0 +1,18 @@
+const { Sequelize } = require('sequelize');
+require('dotenv').config();
+
+const sequelize = new Sequelize(process.env.POSTGRES_DATABASE_URL);
+
+async function testConnection() {
+    try {
+        await sequelize.authenticate({ logging: false });
+        console.log('Connection has been established successfully.');
+    } catch (error) {
+        console.error('Unable to connect to the database:', error);
+    }
+}
+
+module.exports = {
+    testConnection,
+    sequelize
+}
--- index.js
+++ index.js
@@ -1,12 +1,18 @@
 const express = require('express');
+require('dotenv').config();
+const { testConnection } = require('./db/conn');
+
 
 const app = express();
+app.use(express.json());
+
+const PORT = 8080;
+
+testConnection();
 
 app.get('/health', (req, res) => {
     res.send("It's Alive!");
 });
-
-const PORT = 8080;
 
 app.listen(PORT, () => {
     console.log(`App is listening on port ${PORT}`)
