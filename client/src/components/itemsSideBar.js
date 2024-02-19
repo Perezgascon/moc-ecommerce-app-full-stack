c5571a58fa9fc404bdb637096ec00ba2b99f29ac
Perezgascon <76476520+Perezgascon@users.noreply.github.com>
2024-02-19 19:24:45 +0100

    working on sidebar component to dynamically populate categories bubble

--- client/src/components/itemsSideBar.js
+++ client/src/components/itemsSideBar.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import styles from './itemsSideBar.module.css';
-import { getProductCategories} from '../'; // Import the function to fetch categories
+import axios from 'axios'; // Import Axios
 
 export default function ItemsSideBar() {
   const [productCategories, setProductCategories] = useState([]);
@@ -9,8 +9,8 @@ export default function ItemsSideBar() {
     // Fetch product categories when the component mounts
     async function fetchCategories() {
       try {
-        const categories = await getProductCategories();
-        setProductCategories(categories);
+        const response = await axios.get('/categories');
+        setProductCategories(response.data); // Use response.data to access the JSON data
       } catch (error) {
         console.error('Error fetching categories:', error);
       }
@@ -19,7 +19,7 @@ export default function ItemsSideBar() {
   }, []);
 
   return (
-    <div className={styles.sidebar}>
+    <div className={styles.sideBar}>
       <h2>Product Categories</h2>
       <ul>
         {productCategories.map((category, index) => (
