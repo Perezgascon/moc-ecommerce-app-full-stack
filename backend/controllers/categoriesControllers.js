15f2f57ff5cdb1f0198b2a48cd6b33471929d010
Perezgascon <76476520+Perezgascon@users.noreply.github.com>
2024-02-19 18:33:05 +0100

    Used sequelize for my categories controllers

--- backend/controllers/categoriesControllers.js
+++ backend/controllers/categoriesControllers.js
@@ -1,23 +1,24 @@
 // categoriesController.js
 
-require('dotenv').config(); // Load environment variables from .env file
-const { Pool } = require('pg');
-
-const pool = new Pool({
-  connectionString: process.env.POSTGRES_DATABASE_URL,
-});
+const { sequelize } = require('../db/conn');
 
 // Function to fetch product categories from the database
 exports.getCategories = async (req, res) => {
   try {
-    const client = await pool.connect();
-    const query = 'SELECT DISTINCT category FROM products';
-    const result = await client.query(query);
-    client.release();
-    const categories = result.rows.map(row => row.category);
-    res.status(200).json(categories);
+    // Assuming the model for products is defined and exported from another file
+    const { Product }  = require('../models/productsModel');
+
+    // Fetch distinct categories using Sequelize
+    const categories = await Product.findAll({
+      attributes: [[sequelize.fn('DISTINCT', sequelize.col('category')), 'category']]
+    });
+
+    // Extract categories from the result
+    const categoryNames = categories.map(category => category.category);
+
+    res.status(200).json(categoryNames);
   } catch (err) {
     console.error('Error fetching product categories:', err);
-    res.status(500).json({ error: 'Internal Server Error' });
+    res.status(500).json({ error: err.message }); // Send the specific error message
   }
 };
