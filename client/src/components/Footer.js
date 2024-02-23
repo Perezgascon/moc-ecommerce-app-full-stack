68b6a56ca5a923474acd57eddeab2588d7aedd59
Perezgascon <76476520+Perezgascon@users.noreply.github.com>
2024-02-24 00:05:05 +0100

    frontend fixes

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -5,14 +5,14 @@ export default function Footer() {
     return (
         <div>
             <div className={styles.footer}>
-                <ul className={styles.footerLinks}>
+                {/* <ul className={styles.footerLinks}>
                     <li>Faq</li>
                     <li>Condiments</li>
                     <li>Our Story</li>
                     <li>Blog</li>
                     <li>Contact</li>
                 </ul>
-                <p className={styles.footerAttribution}>Website designed by Adolfo Perez-Gascon</p>
+                <p className={styles.footerAttribution}>Website designed by Adolfo Perez-Gascon</p> */}
 
             </div>
         </div>
--- client/src/components/ShoppingCartButton.js
+++ client/src/components/ShoppingCartButton.js
@@ -1,18 +1,25 @@
-import React from 'react'
-import styles from './shoppingCartButton.module.css'
-import { Link } from 'react-router-dom'; // Import Link
+import React from 'react';
+import styles from './shoppingCartButton.module.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCartShopping } from '@fortawesome/free-solid-svg-icons'; // Import the shopping cart icon
 
+export default function ShoppingCartButton({ itemCount, totalPrice }) {
+  const handleClick = () => {
+    // Navigate to the "/checkout" route
+    window.location.href = "/checkout";
+  };
 
-export default function ShoppingCartButton() {
   return (
     <div>
-      <Link to={"/checkout"} className={styles.productLink}>
+      <button onClick={handleClick}>
         <div className={styles.shoppingCart}>
-          <div className={styles.shoppingCartIcon}><i className="fa-solid fa-cart-shopping"></i></div>
-          {/* <div><span className={styles.shoppingCartItemCounter}>{itemCount}</span> items</div>
-      <div>$<span className={styles.shoppingCartPrice}>{totalPrice}</span></div> */}
+          <div className={styles.shoppingCartIcon}>
+            <FontAwesomeIcon icon={faCartShopping} /> {/* Use the Font Awesome shopping cart icon */}
+          </div>
+          <div>{itemCount} items</div>
+          <div>${totalPrice}</div>
         </div>
-      </Link>
+      </button>
     </div>
-  )
+  );
 }
