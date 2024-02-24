6ae424ec0b96243441aed04451850cc3471106c5
Perezgascon <76476520+Perezgascon@users.noreply.github.com>
2024-02-24 01:29:02 +0100

    last minute adjustments

--- client/src/pages/PaymentPage.js
+++ client/src/pages/PaymentPage.js
@@ -1,21 +1,24 @@
-import React from 'react'
-import styles from './paymentPage.module.css'
+import React from 'react';
+import styles from './paymentPage.module.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faPaypal,
+  faStripe
+ } from '@fortawesome/free-brands-svg-icons';
+ import { faMoneyBill, faCreditCard } from '@fortawesome/free-solid-svg-icons';
+ import abalogo from '../assets/abalogo.png';
+
 
 export default function PaymentPage() {
   return (
     <div className={styles.paymentMainContainer}>
       <h1>Choose Your Payment Method</h1>
       <div className={styles.paymentContainer}>
-        <div className={styles.paymentOption}><label class="paypal-icon"><i
-          class="fa-brands fa-cc-paypal payment-option-icon"></i></label></div>
-        <div className={styles.paymentOption}><label class="stripe-icon"><i
-          class="fa-brands fa-stripe payment-option-icon"></i></label></div>
-        <div className={styles.paymentOption}><label class="cash-icon"><i
-          class="fa-solid fa-money-bill-1-wave payment-option-icon"></i></label></div>
-        <div className={styles.paymentOption}><label class="credit-card-icon"><i
-          class="fa-solid fa-credit-card payment-option-icon"></i></label></div>
-        <div className={styles.paymentOption}><img class="payment-option-icon" src="./assets/abalogo.png"></img></div>
+        <div className={styles.paymentOption}><FontAwesomeIcon icon={faPaypal} className={styles.paymentOptionIcon} /></div>
+        <div className={styles.paymentOption}><FontAwesomeIcon icon={faStripe} className={styles.paymentOptionIcon} /></div>
+        <div className={styles.paymentOption}><FontAwesomeIcon icon={faMoneyBill} className={styles.paymentOptionIcon} /></div>
+        <div className={styles.paymentOption}><FontAwesomeIcon icon={faCreditCard} className={styles.paymentOptionIcon} /></div>
+        <div className={styles.paymentOption}><img src={abalogo} className={styles.paymentOptionIcon} /></div>
       </div>
     </div>
-  )
+  );
 }
